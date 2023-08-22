53efea5bab61c3126f5f942e14f9c0642b2d219e
yasodhara <yasodhara369@gmail.com>
2023-08-22 16:43:21 +0530

    varible declaration

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "welcome to devops"
-echo "learning shell script"
+# p1=Ramesh
+# p2=Suresh
+echo "Ramesh: hi suresh how r u"
+echo "Suresh: Iam fine how r u"
+echo " Ramesh Iam fine" 
