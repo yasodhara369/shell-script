11a3a6eeda1823af9ad4a644aae26ff4abbb3410
yasodhara <yasodhara369@gmail.com>
2023-08-22 16:53:03 +0530

    varible declaration

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "welcome to devops"
-# p1=Ramesh
-# p2=Suresh
-echo "Ramesh: hi suresh how r u"
-echo "Suresh: Iam fine how r u"
-echo " Ramesh Iam fine" 
+ p1=Ramesh
+ p2=Suresh
+echo "$p1: hi $p2 how r u"
+echo "$p2: Iam fine how r u $p1"
+echo " $p1 Iam fine" 
